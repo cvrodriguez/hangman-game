47f3688ccc8857e929f964c6614a92415b7aa4e2
cvrodriguez <cinvir15@gmail.com>
2023-01-09 22:01:32 +0100

    funtions to make the hangman body

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Canvas from './comonents/CanvasComponent';
+
 
 function App() {
+  const gras = (context) => {
+    context.beginPath()
+    context.moveTo(20, 230);
+    context.strokeStyle = "#FFFFFF";
+    context.lineTo(100, 230);
+    context.stroke()
+  }
+
+  const head = (context) => {
+    context.beginPath()
+    context.arc(140, 70, 20, 0 * Math.PI, 2 * Math.PI);
+    context.stroke();
+  }
+
+  const rod = (context) => {
+    context.beginPath()
+    context.moveTo(60, 20);
+    context.lineTo(140, 20);
+    context.lineTo(140, 50);
+    context.moveTo(60, 20);
+    context.lineTo(60, 230);
+    context.stroke();
+  }
+
+  const body = (context) => {
+    context.beginPath()
+    context.moveTo(140, 90);
+    context.lineTo(140, 150);
+    context.stroke();
+  }
+
+  const arms = (context) => {
+    context.beginPath()
+    context.moveTo(140, 120);
+    context.lineTo(120, 100);
+    context.moveTo(140, 120);
+    context.lineTo(160, 100);
+    context.stroke();
+  }
+
+  const legs = (context) => {
+    context.beginPath()
+    context.moveTo(140, 150);
+    context.lineTo(120, 180);
+    context.moveTo(140, 150);;
+    context.lineTo(160, 180);
+    context.stroke();
+  }
+
+  const draw = (context) => {
+    gras(context)
+    head(context)
+    rod(context)
+    arms(context)
+    body(context)
+    legs(context)
+
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas draw={draw} height={550} width={550} ></Canvas>
+
     </div>
   );
 }
